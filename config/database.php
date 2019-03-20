80f22f331f5fe043832a8f13b60781beb274bfa2
NadTr <trauflernadine@gmail.com>
2019-03-20 16:56:17 +0100

    heroku database settings

--- config/database.php
+++ config/database.php
@@ -2,8 +2,12 @@
 
 $db;
 if(getenv('APP_ENV'!='local')){
-  $db = parse_url(getenv("DATABASE_URL"));
+  $db = parse_url(env("DATABASE_URL"));
   $db["path"] = ltrim($db["path"], "/");
+  $db["host"];
+  $db["port"];
+  $db["user"];
+  $db["pass"];
 }
 
 return [
