57c6a5a7906d8599f37f552bde469f201a6b22d3
NadTr <trauflernadine@gmail.com>
2019-03-21 10:41:04 +0100

    app/auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
--- routes/api.php
+++ routes/api.php
@@ -13,15 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 //Users
 Route::post('/register', 'AuthController@register');
 Route::post('login', 'AuthController@login');
 
+//if authenticated
 Route::middleware('auth:api')->group(function () {
 
+    Route::->get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
